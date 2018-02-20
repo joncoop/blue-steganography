98e14e3c2fc1687b98c3a09cbaf16545093e2bcc
Jon Cooper <joncoop@users.noreply.github.com>
2018-02-20 18:34:43 -0500

    Refactored

--- blue_steganography.py
+++ blue_steganography.py
@@ -1,4 +1,5 @@
 import pygame
+from itertools import product
 
 pygame.init()
 
@@ -44,45 +45,53 @@ def binary_to_text(binary_str):
   return result
 
 def hide_message(message_file_path, original_image_path, encoded_image_path):
+  '''
+  Hides a secret message inside an image file.
+  '''
+
   # read message from text file and append stop flag
   with open(message_file_path, 'r') as f:
     message = f.read()
   message += stop_flag
 
   # convert message to binary
-  binary_digits = text_to_binary(message)
+  binary_str = text_to_binary(message)
     
   # load the original image file as a surface
   surf = pygame.image.load(original_image_path)
   width = surf.get_width()
   height = surf.get_height()
 
-  # loop through surface and adjust pixels
-  digit_index = 0
-  result = ""
-  for x in range(width):
-    for y in range(height):
-      loc = [x, y]
+  # loop through message and adjust pixels
+  i = 0
+  x = 0
+  y = 0 
       
-      if digit_index < len(binary_digits):
-        color = surf.get_at(loc)
+  while i < len(binary_str):
+    loc = [x, y]
+    color = surf.get_at(loc)
 
-        blue = color.b
-        bit = int(binary_digits[digit_index])
-        even = blue % 2 == 0
+    blue = color.b
+    bit = int(binary_str[i])
+    even = blue % 2 == 0
         
-        if (even and bit == 1) or (not even and bit == 0):
-          blue += 1
+    if (even and bit == 1) or (not even and bit == 0):
+      blue += 1
 
-          if blue > 255:
-            blue -= 2
-            
-        color.b = blue
-        result += str(color.b % 2)
+      if blue > 255:
+        blue -= 2
         
-        surf.set_at(loc, color)
-        digit_index += 1
+      color.b = blue
+    
+      surf.set_at(loc, color)
+    
+    i += 1
+    x += 1
 
+    if x == width:
+      x = 0
+      y += 1
+    
   # save the new image
   pygame.image.save(surf, encoded_image_path)
 
@@ -90,6 +99,10 @@ def hide_message(message_file_path, original_image_path, encoded_image_path):
 
 
 def extract_message(encoded_image_path, extracted_message_path):
+  '''
+  Extracts a secret message from an image file.
+  '''
+
   # load image as surface
   surf = pygame.image.load(encoded_image_path)
   width = surf.get_width()
@@ -98,23 +111,23 @@ def extract_message(encoded_image_path, extracted_message_path):
   # build binary digit string from image
   binary_str = ""
   
-  for x in range(width):
-    for y in range(height):
+  for y in range(height):
+    for x in range(width):
       loc = [x, y]
       color = surf.get_at(loc)
       blue = color.b
       binary_str += str(blue % 2)
 
   # convert binary string to text
-  result = binary_to_text(binary_str)
+  message = binary_to_text(binary_str)
   
   # truncate any characters after stop flag
-  end = result.find(stop_flag)
-  result = result[:end]
+  end = message.find(stop_flag)
+  message = message[:end]
   
   # write extracted message to file
   with open(extracted_message_path, 'w') as f:
-    f.write(result)
+    f.write(message)
         
   print("Success! Your secret message was extracted to '" + extracted_message_path + "'.")
 
